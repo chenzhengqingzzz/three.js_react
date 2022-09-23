729518198a16aab0e93b721102436054cb79b08c
陈正清macbook pro <tenchenzhengqing@qq.com>
2022-09-24 01:56:44 +0800

    Animations

---
+++ src/public/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Scene from './components/Scene'
+import Test from './components/Test'
+import TestScene from './components/TestScene'
+
+export default function App() {
+  return (
+    <div>
+      <Scene/>
+      {/* <Test/> */}
+    </div>
+  )
+}
